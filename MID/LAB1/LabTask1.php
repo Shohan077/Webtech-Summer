4edb47644605674ca2dc994b3fc85a4f5539f9d6
unknown <khandakarshohan77@gmail.com>
2025-07-20 10:45:38 +0600

    Lab 1

---
+++ MID/LAB1/LabTask1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+    <title>Registation Form</title>
+<body>
+<center>
+
+<h1 style = "color: blue">AIUB</h1>
+
+<h2 style = "color: green">Please fill the form</h2>
+
+</center>
+
+<h3>Please Enter Your Information</h3>
+
+<table>
+    <tr>
+    <td>
+    Enter your name </td> <td> <input type="text"> </td>
+    </tr>
+
+<tr>
+    <td>
+        Enter your age </td> <td> <input type="text"> </td>
+    </td>
+</tr>
+
+<tr>
+    <td>Choose your language :</td>
+    <td><input type="radio" name="des" > Bangla </td>
+    <td><input type="radio" name="des" > English </td>
+</tr>
+
+<tr>
+    <td> Select your date :</td>
+    <td><input type = "date"></td>
+</tr>
+<tr>
+    <td><input type="Submit" name= "Submit"></td>
+</tr>
+</table>
+
+
+
+
+
+
+</body>
+</html>
